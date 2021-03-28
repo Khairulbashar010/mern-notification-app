0568ceabf88b96c414d0f496c2773aa5c734e6fd
Khairul Bashar <khairul.bashar.rpsu@gmail.com>
2021-03-28 22:05:13 +0600

    finally

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(bodyParser.json());
 // Database Connection
 
 mongoose
-	.connect(process.env.databaseURI, {
+	.connect(process.env.MONGODB_URI, {
 		useNewUrlParser: true,
 		useUnifiedTopology: true,
 		useCreateIndex: true,
