a6accde3289ff736705219db0fc5684413855d87
Khairul Bashar <khairul.bashar.rpsu@gmail.com>
2021-03-31 18:36:21 +0600

    cors

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ if(process.env.NODE_ENV == "production"){
 	const path = require('path')
 	app.use(express.static(path.join(__dirname, '/webapp/build')))
 
-	app.get('*', (req, res) => {
+	app.get('*', cors(),	(req, res) => {
 		res.sendFile(path.join(__dirname, 'webapp', 'build', 'index.html'))
 	});
 }
