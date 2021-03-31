49ef41e2dd09fe38be14fab8eedd0164513b38a6
Khairul Bashar <khairul.bashar.rpsu@gmail.com>
2021-03-31 20:06:29 +0600

    cors

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ app.use(bodyParser.json());
 app.use(function(req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	res.header('Content-Security-Policy', "img-src 'self'");
 	next();
   });
 // Routes
