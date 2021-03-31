7f009f239d401798265ca7f36f2568e64945c32b
Khairul Bashar <khairul.bashar.rpsu@gmail.com>
2021-03-31 20:38:21 +0600

    cors

--- server.js
+++ server.js
@@ -16,17 +16,14 @@ var allowCrossDomain = function(req,res,next) {
     res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
 	res.header('Content-Security-Policy', "img-src 'self'");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
     next();
 };
 app.use(allowCrossDomain);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use(function(req, res, next) {
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-	next();
-});
 // Routes
 
 app.use("/", userRoute);
